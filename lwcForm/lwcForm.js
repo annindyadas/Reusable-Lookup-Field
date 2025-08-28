f2560f0c2115800e2a56bf2ff34b03764774ab1f
Akash <annindyadas@gmail.com>
2025-08-28 13:43:37 -0400

    Update lwcForm.js

--- lwcForm/lwcForm.js
+++ lwcForm/lwcForm.js
@@ -1,3 +1,9 @@
 import { LightningElement, api, track } from 'lwc';
 
-export default class LwcForm extends LightningElement {}
+export default class LwcForm extends LightningElement {
+      handleAccountSelect(event){
+        // Access the recordId which will contain either a single ID or comma-separated IDs
+        let recordId = event.detail.recordId;
+        console.log('Selected account id(s):', recordId);
+    }   
+}
